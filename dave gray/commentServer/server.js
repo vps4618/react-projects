fcee705d56158af5512fac22492b1fb68b0f970c
vishwa praveen <vishwapraveen4618@gmail.com>
2022-11-21 09:06:32 +0530

    delete functionality added

--- dave gray/commentServer/server.js
+++ dave gray/commentServer/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require("./database");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv/config");
 
 const PORT = 8080 || process.env.PORT;
 const host = "localhost";
@@ -12,36 +13,56 @@ app.use(bodyParser.json());
 // disable CORS error
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PATCH", "DELETE"],
+    origin: "https://vps4618.github.io",
+    methods: ["GET", "POST","DELETE"],
   })
 );
 
 // index page get request handling
 app.get("/", (req, res) => {
-  res.send("<h3>Welcome to hand cricket comment page !</h3>");
+  res.send("<h3>Welcome to hand Cricket Comment page !</h3>");
 });
 
 // post request  handling
 app.post("/api/comments", (req, res) => {
   try {
-    const { firstName, lastName, comment } = req.body;
+    const { firstName, lastName, comment, dateTime } = req.body;
 
-    const sql =
-      "INSERT INTO handCricketComments (firstName,lastName,comment) VALUES (?,?,?)";
-    const params = [firstName, lastName, comment];
-    db.run(sql, params, (err) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
+    if (firstName === "delete") {
+      let id = lastName;
+      let enterkey = comment;
+      if (enterkey === process.env.enterKey) {
+        db.run(
+          "DELETE FROM handCricketComments WHERE id=?",
+          id,
+          function (err) {
+            if (err) {
+              res.status(400).json({ error: err.message });
+            } else {
+              res.status(200).json({ message: "item deleted successfully !" });
+            }
+          }
+        );
       } else {
-        res.status(201).json({
-          message: `${
-            firstName + " " + lastName
-          }'s comment added successfully !`,
-          customerId: this.lastID,
-        });
+        res.status(400).json({ error: "Invalid key !" });
       }
-    });
+    } else {
+      const sql =
+        "INSERT INTO handCricketComments (firstName,lastName,comment,dateTime) VALUES (?,?,?,?)";
+      const params = [firstName, lastName, comment, dateTime];
+      db.run(sql, params, (err) => {
+        if (err) {
+          res.status(400).json({ error: err.message });
+        } else {
+          res.status(201).json({
+            message: `${
+              firstName + " " + lastName
+            }'s comment added successfully !`,
+            customerId: this.lastID,
+          });
+        }
+      });
+    }
   } catch (err) {
     res.status(400).json({ error: err.toString() });
   }
@@ -65,47 +86,47 @@ app.get("/api/comments", (req, res) => {
 });
 
 // update request handling
-app.put("/api/comments/:id", (req, res) => {
-  try {
-    const { firstName, lastName, comment } = req.body;
-    const sql = `UPDATE handCricketComments set firstName = ?,lastName = ?,comment = ? WHERE id = ?`;
-    const id = req.params.id;
-    const params = [firstName, lastName, comment, id];
+// app.put("/api/comments/:id", (req, res) => {
+//   try {
+//     const { firstName, lastName, comment,dateTime } = req.body;
+//     const sql = `UPDATE handCricketComments set firstName = ?,lastName = ?,comment = ?,dateTime = ? WHERE id = ?`;
+//     const id = req.params.id;
+//     const params = [firstName, lastName, comment, id,dateTime];
 
-    // we must use normal function instead of arrow function for get this.changes
+// we must use normal function instead of arrow function for get this.changes
 
-    db.run(sql, params, function (err) {
-      if (err) {
-        res.status(400).json({ error: err.message });
-      } else {
-        res.status(200).json({
-          updated: this.changes,
-        });
-      }
-    });
-  } catch (err) {
-    res.status(400).json({ error: err.toString() });
-  }
-});
+//     db.run(sql, params, function (err) {
+//       if (err) {
+//         res.status(400).json({ error: err.message });
+//       } else {
+//         res.status(200).json({
+//           updated: this.changes,
+//         });
+//       }
+//     });
+//   } catch (err) {
+//     res.status(400).json({ error: err.toString() });
+//   }
+// });
 
 // delete request handling
-app.delete("/api/comments/:id", (req, res) => {
-  try {
-    db.run(
-      "DELETE FROM handCricketComments WHERE id=?",
-      req.params.id,
-      function (err) {
-        if (err) {
-          res.status(400).json({ error: err.message });
-        } else {
-          res.status(200).json({ message: "item deleted successfully !" });
-        }
-      }
-    );
-  } catch (err) {
-    res.status(400).json({ error: err.toString() });
-  }
-});
+// app.delete("/api/comments/:id", (req, res) => {
+//   try {
+//     db.run(
+//       "DELETE FROM handCricketComments WHERE id=?",
+//       req.params.id,
+//       function (err) {
+//         if (err) {
+//           res.status(400).json({ error: err.message });
+//         } else {
+//           res.status(200).json({ message: "item deleted successfully !" });
+//         }
+//       }
+//     );
+//   } catch (err) {
+//     res.status(400).json({ error: err.toString() });
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://${host}/${PORT}`);
