14a12974cd97e820dbc8b7a07cf181824dc0dfc5
vishwa praveen <vishwapraveen4618@gmail.com>
2022-11-21 12:48:32 +0530

    remove spaces added and thank you comment added

--- dave gray/commentServer/server.js
+++ dave gray/commentServer/server.js
@@ -34,8 +34,14 @@ app.post("/api/comments", (req, res) => {
     let firstName1 = striptags(firstName);
     let lastName1 = striptags(lastName);
     let comment1 = striptags(comment);
+
+    // remove spaces
+    firstName1 = firstName1.trim();
+    lastName1 = lastName1.trim();
+    comment1 = comment1.trim();
+
     let isDev = false;
-    
+
     if (firstName1 === "delete") {
       let id = lastName1;
       let enterkey = comment1;
@@ -55,11 +61,13 @@ app.post("/api/comments", (req, res) => {
         res.status(400).json({ error: "Invalid key !" });
       }
     } else {
-      
-      if(firstName1 === process.env.devName){
+      if (firstName1 === process.env.devName) {
         firstName1 = "<font style='color:gold'>Vps4618<sup>dev</sup></font>";
         lastName1 = "";
-        comment1 = "<font style='color:maroon;font-weight:bold;'>" + comment1+"</font>";
+        comment1 =
+          "<font style='color:maroon;font-weight:bold;'>" +
+          comment1 +
+          "</font>";
         isDev = true;
       }
 
@@ -70,45 +78,62 @@ app.post("/api/comments", (req, res) => {
         if (err) {
           res.status(400).json({ error: err.message });
         } else {
-          if(!isDev){
-          // async..await is not allowed in global scope, must use a wrapper
-          async function main() {
-            // Generate test SMTP service account from ethereal.email
-            // Only needed if you don't have a real mail account for testing
-            let testAccount = await nodemailer.createTestAccount();
+          if (!isDev) {
+            // async..await is not allowed in global scope, must use a wrapper
+            async function main() {
+              // Generate test SMTP service account from ethereal.email
+              // Only needed if you don't have a real mail account for testing
+              let testAccount = await nodemailer.createTestAccount();
 
-            // create reusable transporter object using the default SMTP transport
-            let transporter = nodemailer.createTransport({
-              service: "gmail",
-              auth: {
-                user: "handCricketComment4618@gmail.com", // generated ethereal user
-                pass: process.env.appPwd, // gmail generated app password
-              },
-            });
+              // create reusable transporter object using the default SMTP transport
+              let transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                  user: "handCricketComment4618@gmail.com", // generated ethereal user
+                  pass: process.env.appPwd, // gmail generated app password
+                },
+              });
 
-            // send mail with defined transport object
-            let info = await transporter.sendMail({
-              from: `"Hand Cricket Comment" <handCricketComment4618@gmail.com>`, // sender address
-              to: "vishwapraveen4618@gmail.com", // list of receivers
-              subject: `${firstName1} comment`, // Subject line
-              // text: `Name : ${firstName + " "  +lastName} \nComment : ${comment}\nDate : ${dateTime}`, // plain text body
-              html: `<label style="font-size:20px;font-weight:bold;">Name</label> : <label style="font-size:15px">${firstName1 + " "  +lastName1}</label><br>
+              // send mail with defined transport object
+              let info = await transporter.sendMail({
+                from: `"Hand Cricket Comment" <handCricketComment4618@gmail.com>`, // sender address
+                to: "vishwapraveen4618@gmail.com", // list of receivers
+                subject: `${firstName1} comment`, // Subject line
+                // text: `Name : ${firstName + " "  +lastName} \nComment : ${comment}\nDate : ${dateTime}`, // plain text body
+                html: `<label style="font-size:20px;font-weight:bold;">Name</label> : <label style="font-size:15px">${
+                  firstName1 + " " + lastName1
+                }</label><br>
               <br>
               <label style="font-size:20px;font-weight:bold;">Comment</label> : <label style="font-size:15px">${comment1}</label>
               <br>
               <br>
               <label style="font-size:20px;font-weight:bold;">Date</label> : <label style="font-size:15px">${dateTime}</label>`, // html body
-            });
+              });
+            }
 
-          }
-
-          main().catch(console.error);
+            main().catch(console.error);
           }
           res.status(201).json({
             message: `${
               firstName1 + " " + lastName1
             }'s comment added successfully !`,
             customerId: this.lastID,
+          });
+
+          // dev comment
+          let devName = "<font style='color:gold'>Vps4618<sup>dev</sup></font>";
+          let devMessage =
+            "<font style='color:maroon;font-weight:bold;'>" +
+            `Thank you ${firstName1 + " " + lastName1} 🧡` +
+            "</font>";
+          let devLastName = "";
+          let devParams = [devName, devLastName, devMessage,dateTime];
+          db.run(sql, devParams, (err) => {
+            if (err) {
+              res.status(400).json({ error: err.message });
+            } else {
+              res.status(201);
+            }
           });
         }
       });
